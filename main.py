b580dc05b3345c39d5e2de238786b924dc7f6e6c
Dmitrii Mazilkin <dim.mazilkin@gmail.com>
2025-06-25 19:01:19 +0200

    Update main with example and add plotting result, fix fit method with SGD.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from typing import Tuple
 
 from src.svm import MySVM
@@ -11,14 +12,26 @@ def create_data() -> Tuple[pd.DataFrame, pd.Series]:
     
     X = pd.DataFrame(np.zeros((N, 2)) + 10 * np.random.rand(N, 2))
     Y = (X.loc[:, 0] > X.loc[:, 1] - 2).astype(int)
-    X = pd.concat([X, X.loc[:, 1]**2, X.loc[:, 1]**3], axis=1)
     
     return X, Y
 
 def main():
     X, Y = create_data()
-    model = MySVM()
-    model.fit(X, Y)
+    
+    model = MySVM(n_iter=10)
+    model.fit(X, Y, verbose=True)
+    Y_predicted = model.predict(X)
+    
+    figure, axis = plt.subplots(1, 2)
+    axis[0].scatter(X.to_numpy()[Y == 1][:, 0], X.to_numpy()[Y == 1][:, 1], color='blue')
+    axis[0].scatter(X.to_numpy()[Y == 0][:, 0], X.to_numpy()[Y == 0][:, 1], color='red')
+    axis[0].set_title('Real data')
+    axis[1].scatter(X.to_numpy()[Y_predicted == 1][:, 0], X.to_numpy()[Y_predicted == 1][:, 1], color='blue')
+    axis[1].scatter(X.to_numpy()[Y_predicted == 0][:, 0], X.to_numpy()[Y_predicted == 0][:, 1], color='red')
+    axis[1].set_title('Predicted data')
+    plt.show()
+    
+    print(model.get_coef())
     
 if __name__ == '__main__':
     main()
--- src/svm.py
+++ src/svm.py
@@ -1,20 +1,13 @@
 import pandas as pd
 import numpy as np
-from typing import Union
+from typing import Union, Tuple
 
 class MySVM:
-    def __init__(self, n_iter: int = 10, learning_rate: float = 0.1, metric: Union[None, str] = None, reg: Union[None, str] = None, l1_coef: float = 0.0, l2_coef: float = 0.0, sgd_sample: Union[int, float, None] = None, random_state: int = 42):
+    def __init__(self, n_iter: int = 10, learning_rate: float = 0.001):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = None
         self.b = None
-        self.metric = metric
-        self.best_metric = None
-        self.reg = reg
-        self.l1_coef = l1_coef
-        self.l2_coef = l2_coef
-        self.sgd_sample = sgd_sample
-        self.random_state = random_state
 
     def __str__(self):
         return f"MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
@@ -22,24 +15,62 @@ class MySVM:
     def fit(self, X_input: pd.DataFrame, Y_input: pd.Series, verbose: Union[bool, int] = False):
         Y = self._preprocess_labels(Y_input)
         X = self._preprocess_features(X_input)
-        self.weights = np.zeros((X_input[0], 1)) + 1
-        self.b = 1
+        self.weights = np.ones((X_input.shape[1], 1))
+        samples_count = Y.shape[0]
+        self.b = 1.0
         iter = 0
         
-        while iter < self.n_iter:
+        while iter < self.n_iter:            
+            for i in range(samples_count):
+                X_i = np.array([X[i, :]])
+                Y_i = float(Y[i])
+                Y_predict = float(self._predict(X_i, Y_i))
+                
+                gradient_weights, gradient_bias = self._calc_gradient(X_i, Y_i, Y_predict)
+                self.weights -= self.learning_rate * gradient_weights
+                self.b -= self.learning_rate * gradient_bias
+            
             loss = self._calc_loss(X, Y)
+            if (verbose > 0) and (iter % verbose == 0):
+                ind_log = 'start' if iter == 0 else iter
+                print(f'ind_log | loss: {loss}')
             iter += 1
         
     def _preprocess_labels(self, Y_input: pd.Series) -> np.array:
-        pos, neg = Y_input.unique()
         Y_preprocessed = pd.Series(np.zeros(Y_input.shape))
-        Y_preprocessed[Y_input == pos] = 1
-        Y_preprocessed[Y_input == neg] = -1
-        return Y_preprocessed.to_numpy()
+        Y_preprocessed[Y_input == 1] = 1
+        Y_preprocessed[Y_input == 0] = -1
+        return Y_preprocessed.to_numpy().reshape(-1, 1)
     
     def _preprocess_features(self, X_input: pd.DataFrame) -> np.array:
         return X_input.to_numpy()
     
+    def _predict(self, X: np.array, Y: np.array) -> np.array:
+        return Y * (X @ self.weights + self.b)
+    
     def _calc_loss(self, X: np.array, Y: np.array) -> float:
+        predict = self._predict(X, Y)
+        wrong_ind = predict < 1
+        loss = np.sum(self.weights**2)
+
+        if sum(wrong_ind):
+            loss += np.sum(1 - predict[wrong_ind]) / Y.shape[0]
+        return loss
+    
+    def _calc_gradient(self, X: np.array, Y: np.array, Y_predict: np.array) -> Tuple[np.array, float]:
+        grad_weights = 2 * self.weights
+        grad_bias = 0
         
-        
+        if  Y_predict < 1:
+            grad_weights -= X.T * Y
+            grad_bias -= Y
+        return grad_weights, grad_bias
+    
+    def get_coef(self):
+        return self.weights.ravel(), self.b
+    
+    def predict(self, X_input: pd.DataFrame):
+        X = self._preprocess_features(X_input)
+        predict = np.array(np.sign((X @ self.weights + self.b)), dtype=int)
+        predict[predict == -1] = 0
+        return predict.ravel()
